c7ed8143920eb2e27ac9c6f2a99b46682a62562d
luoPython <njus@msn.cn>
2016-09-22 21:05:16 -0400

    Created by Coding Ground Client

---
+++ New Project-20160922/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld
+{   public static final String INPUT = "I  hate you.";
+    public static void main(String[] argus)
+    {
+        System.out.println("The origin sentence was: "+INPUT);
+        int position = INPUT.indexOf("hate",0);
+        String changedOne = INPUT.substring(0,position);
+        String changedOneDash = changedOne.trim();
+        String changedTwo = INPUT.substring(position+"hate".length());
+        String changedTwoDash = changedTwo.trim();//去掉多鱼的空格
+        String changed = changedOneDash+" LOVE "+changedTwoDash;
+        //String after = test.toLowerCase();
+        System.out.println("The modified sentence is: "+changed.toUpperCase());
+    }
+}
